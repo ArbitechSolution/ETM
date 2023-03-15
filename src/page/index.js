e1b518b0efe930b73bdf4c04f5094ff44dcdade1
adeelahmed057 <adeelnazir2018@gmail.com>
2023-03-15 16:27:52 +0500

    header and footer

---
+++ src/page/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from '../component/footer/footer'
+import Header from '../component/header/header'
+import "./mainPage.css"
+
+function MainPage() {
+  return (
+    <>
+    <Header/>
+    <div style={{background:"#32065B"}}>
+        <Footer/>
+    </div>
+    </>
+  )
+}
+
+export default MainPage
